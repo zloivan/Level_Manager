0b1cb096a2eff462346637f614c080d35c946fc5
Ivan Khomichkov <i.homichkov@wannaplay.gg>
2022-10-24 12:54:47 +0400

    merge test project and LevelManager project

---
+++ Assets/MechanicsSystems/Patterns/PatternRootRunner.cs
@@ -0,0 +1,27 @@
+using System;
+using LevelManager.MechanicsSystems.Patterns.Prototype;
+using UnityEngine;
+
+namespace LevelManager.MechanicsSystems
+{
+    public class PatternRootRunner : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Run from start");
+            PrototypeRunner.Start();
+        }
+
+        private void Awake()
+        {
+            Debug.Log("Run from awake");
+            PrototypeRunner.Awake();
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log("Run from OnEnables");
+            PrototypeRunner.OnEnable();
+        }
+    }
+}
